6444e7d37fd7d0063571e0e20a1555881ccd3043
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-17 21:55:12 +0100

    Change welcome message

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Cw1.Calculator!");
+            Console.WriteLine("Welcome to Cw1.Calculator! Please listen to instructions...");
             Console.Write("Please input a number for the first value: ");
             double firstValue = double.Parse(Console.ReadLine());
             Console.Write("Please input a number for the second value: ");
