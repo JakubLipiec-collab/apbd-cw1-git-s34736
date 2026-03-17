6f2a3e07a964ff75d7837707d6b72b656ac5a3f8
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-17 20:32:56 +0100

    Add Calc class

---
+++ Cw1/Calc.cs
@@ -0,0 +1,16 @@
+﻿namespace Cw1;
+
+public class Calc
+{
+    private double a;
+    private double b;
+    public Calc(double a, double b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+    public double Calculate(string operation)
+    {
+        return 0;
+    }
+}
