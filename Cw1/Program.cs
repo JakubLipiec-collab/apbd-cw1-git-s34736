7de7534a3a4d39c3d3415e893d75a07e63e44047
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-17 20:42:58 +0100

    Add basic txt interface

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, User!");
+            Console.Write("Please input a number for the first value: ");
+            double firstValue = double.Parse(Console.ReadLine());
+            Console.Write("Please input a number for the second value: ");
+            double secondValue = double.Parse(Console.ReadLine());
+            Console.Write("Please input an operator(+,-,/,*): ");
+            string op = Console.ReadLine();
+            Calc c = new Calc(firstValue, secondValue);
+        }
+    }
+}
